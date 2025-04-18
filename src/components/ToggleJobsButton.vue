ad5f033f79864d2ba3a3f337d7fcde722946dd04
Yassine <yassineelbrahmi11@gmail.com>
2025-04-18 14:07:23 +0200

    feat: Add ToggleJobsButton component for job listing toggle functionality

---
+++ src/components/ToggleJobsButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="m-auto max-w-lg my-10 px-6 text-center">
+    <button
+      @click="$emit('toggle')"
+      class="block bg-black text-white py-4 px-6 rounded-xl hover:bg-gray-700 m-auto"
+    >
+      {{ showAll ? "Show Less" : "View All Jobs" }}
+    </button>
+  </section>
+</template>
+
+<script setup>
+defineProps({
+  showAll: Boolean,
+});
+
+defineEmits(["toggle"]);
+</script>
