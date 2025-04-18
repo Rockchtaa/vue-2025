a6c50dbc8a77af74f88845d783c044de6ebabe69
Yassine <yassineelbrahmi11@gmail.com>
2025-04-18 10:18:37 +0200

    feat: Initialize Vue.js project with essential components and routing
    - Added main application structure in App.vue and App2.vue
    - Implemented Navbar and HeroSec components for navigation and landing section
    - Created main CSS file with Tailwind CSS setup
    - Integrated job data in JSON format for job listings
    - Set up Vue Router for navigation
    - Configured Vite for development server and proxy settings
    - Added toast notifications using vue-toastification
    - Included logo image in assets
    - Established a responsive layout with Tailwind CSS

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import HeroSec from '@/components/HeroSec.vue';
+import { RouterView } from 'vue-router';
+import { ref } from 'vue';
+const title = ref('Vue Jobs');
+const paragraph = ref('Find your dream job in the Vue.js ecosystem!');
+
+
+</script>
+
+<template>
+  <Navbar />
+  <HeroSec :title="title" :paragraph="paragraph"/>
+  <RouterView />
+</template>
