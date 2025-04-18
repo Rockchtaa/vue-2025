21f6f308c1f1a77ced9e2442ff75f5dc172fb5b2
Yassine <yassineelbrahmi11@gmail.com>
2025-04-18 14:07:44 +0200

    refactor: Remove unused imports and simplify App.vue structure

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <script setup>
-import Navbar from '@/components/Navbar.vue';
-import HeroSec from '@/components/HeroSec.vue';
-import { RouterView } from 'vue-router';
-import { ref } from 'vue';
-const title = ref('Vue Jobs');
-const paragraph = ref('Find your dream job in the Vue.js ecosystem!');
-
-
+import Navbar from '@/components/Navbar.vue'
 </script>
 
 <template>
   <Navbar />
-  <HeroSec :title="title" :paragraph="paragraph"/>
-  <RouterView />
+  <router-view />
 </template>
