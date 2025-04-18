042004f72e82837091981f89e671320bde9b66a8
Yassine <yassineelbrahmi11@gmail.com>
2025-04-18 14:07:33 +0200

    feat: Add JobListing component to display a list of jobs with JobCard

---
+++ src/components/JobListing.vue
@@ -0,0 +1,28 @@
+<script setup>
+import JobCard from '@/components/JobCard.vue'
+defineProps({
+  jobs: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <section class="bg-green-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div
+          v-for="job in jobs"
+          :key="job.id"
+          class="bg-white rounded-xl shadow-md relative"
+        >
+          <JobCard :job="job" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
