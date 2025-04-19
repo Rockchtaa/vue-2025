57c5f61c24461e1318eaa73814448f5a345f7dac
Yassine <yassineelbrahmi11@gmail.com>
2025-04-19 08:46:39 +0200

    feat: Refactor Navbar component to use dynamic class binding for active route highlighting

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, useRoute } from "vue-router";
 import logo from "@/assets/img/logo.png";
+const route = useRoute();
 </script>
 
 <template>
@@ -17,24 +18,41 @@ import logo from "@/assets/img/logo.png";
               >Vue Jobs</span
             >
           </RouterLink>
-          
+
           <div class="md:ml-auto">
             <div class="flex space-x-2">
               <RouterLink
                 to="/"
-                class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >Home</RouterLink
+                :class="[
+                  'text-white',
+                  route.path === '/' ? 'bg-gray-900' : 'hover:bg-gray-900',
+                  'rounded-md px-3 py-2',
+                ]"
               >
+                Home
+              </RouterLink>
               <RouterLink
                 to="/jobs"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
-                >Jobs</RouterLink
+                :class="[
+                  'text-white',
+                  route.path === '/jobs' ? 'bg-gray-900' : 'hover:bg-gray-900',
+                  'rounded-md px-3 py-2',
+                ]"
               >
+                Jobs
+              </RouterLink>
               <RouterLink
                 to="/add-job"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
-                >Add Job</RouterLink
+                :class="[
+                  'text-white',
+                  route.path === '/add-job'
+                    ? 'bg-gray-900'
+                    : 'hover:bg-gray-900',
+                  'rounded-md px-3 py-2',
+                ]"
               >
+                Add Job
+              </RouterLink>
             </div>
           </div>
         </div>
