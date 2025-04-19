03fb495745afa3e772f169bed0b14e39890f397d
Yassine <yassineelbrahmi11@gmail.com>
2025-04-19 08:47:02 +0200

    feat: Add route for JobsWithID view to enable job detail navigation

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import JobsView from '../views/JobsView.vue';
 import ToggleJobsButton from '../components/ToggleJobsButton.vue'
+import JobsViewWithID from '@/views/JobsViewWithID.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
   { path: '/jobs', name: 'Jobs', component: JobsView },
+  { path: '/jobs/:id', name: 'JobsWithID', component: JobsViewWithID },
 ]
 
 const router = createRouter({
